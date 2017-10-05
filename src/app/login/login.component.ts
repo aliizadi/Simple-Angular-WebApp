71487ad4b8391c9ca7558b30c30fa1c6e238d6d6
aliiz <aliizadi2030@gmail.com>
2017-10-05 19:15:55 +0330

    login component

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { MdDialog, MdDialogRef } from '@angular/material';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  
+  user = { 
+    remember:false
+  };
+
+  constructor(private dialogRef: MdDialogRef<LoginComponent>) { }
+
+  ngOnInit() {
+  }
+
+  onSubmit() {
+    console.log("User: ", this.user)
+    this.dialogRef.close();
+  }
+
+}
