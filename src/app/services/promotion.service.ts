f2d9c1a5daf507add6545e60377dfd481afbac31
aliiz <aliizadi2030@gmail.com>
2017-10-05 19:17:08 +0330

    added all services that get details from server

---
+++ src/app/services/promotion.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+
+import { Promotion } from '../shared/promotion';
+import { PROMOTIONS } from '../shared/promotions';
+
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/operator/delay';
+
+@Injectable()
+export class PromotionService {
+
+  constructor() { }
+
+  getPromotions(): Observable<Promotion[]> {
+    return Observable.of(PROMOTIONS).delay(2000);
+  }
+
+  getPromotion(id: number): Observable<Promotion> {
+    return Observable.of(PROMOTIONS.filter((promo) => (promo.id === id))[0]).delay(2000); 
+  }
+
+  getFeaturedPromotion(): Observable<Promotion> {
+    return Observable.of(PROMOTIONS.filter((promo) => (promo.featured))[0]).delay(2000); 
+  }
+
+}
