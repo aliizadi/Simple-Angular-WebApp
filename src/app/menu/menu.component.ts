02a6c9bd82967d65633f370780f41e2f787d8c31
aliiz <aliizadi2030@gmail.com>
2017-10-05 19:16:12 +0330

    menu component

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Inject } from '@angular/core';
+
+import { Dish } from '../shared/dish';
+
+import { DishService } from '../services/dish.service';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+
+  dishes: Dish[]; 
+  errMess : string;
+  
+  constructor(private dishService: DishService,
+    @Inject('BaseURL') private baseURL) { }
+
+  ngOnInit() {
+    this.dishService.getDishes()
+      .subscribe( dishes => this.dishes = dishes,
+                  errmess => this.errMess = <any>errmess); 
+  }
+
+}
